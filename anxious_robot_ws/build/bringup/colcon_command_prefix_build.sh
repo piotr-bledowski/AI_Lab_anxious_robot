5b06c1410fb1f7808c1a239ceb42aa134f2148ad
Piotr <piotr.bledowski.77@gmail.com>
2023-09-19 22:51:44 +0200

    Added a separate URDF for Gazebo related tags and switched the diff drive control plugin to a relevant one

--- anxious_robot_ws/build/bringup/colcon_command_prefix_build.sh
+++ anxious_robot_ws/build/bringup/colcon_command_prefix_build.sh
@@ -1,1 +1,3 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/piotr/Desktop/AI_Lab/ROS/AI_Lab_anxious_robot/anxious_robot_ws/install/robot_description/share/robot_description/package.sh"
+. "/home/piotr/Desktop/AI_Lab/ROS/AI_Lab_anxious_robot/anxious_robot_ws/install/simulation/share/simulation/package.sh"
